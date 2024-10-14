6244bfb3952cda94b213b0efaf698c8c7f2e14bf
sachin panchal <sachinpanchal9942@gmail.com>
2024-10-14 17:30:05 +0530

    Learn about constructor and destructor

--- C++ OOPs/Basic.cpp
+++ C++ OOPs/Basic.cpp
@@ -1,53 +1,119 @@
 #include<iostream>
 using namespace std;
 
-class Student
-{
-     // If we not define access modifier then by default will be private.
-    string name;
-    int age;
+// class Student
+// {
+//      // If we not define access modifier then by default will be private.
+//     string name;
+//     int age;
 
-    public:
-    void set_name(string name1){ // Setter
-        name = name1;
-    }
+//     public:
+//     void set_name(string name1){ // Setter
+//         name = name1;
+//     }
     
-    void set_age(int age1){
-        if(age1 > 18 && age1 < 70){
-            age = age1;
-        }
-        else{
-            cout << "invalid age" << endl;
-        }
+//     void set_age(int age1){
+//         if(age1 > 18 && age1 < 70){
+//             age = age1;
+//         }
+//         else{
+//             cout << "invalid age" << endl;
+//         }
+//     }
+
+//     string get_name(int password){ // Getter, if password is correct then it will return name.
+//         if(password == 123){
+//             return name;
+//         }
+//         return "";
+//     }
+
+//     int get_age(){
+//         return age;
+//     }
+// };
+
+// int main(){
+//     Student s1;
+//     // s1.name = "john"; // if public then access is direct.
+//     // s1.age = 30;
+
+//     // Student s2;
+//     // s2.name = "brok";
+//     // s2.age = 32;
+
+// // if Access modifier are private then GETTER, SETTER used to get and set the data.
+
+// s1.set_name("sachin");
+// s1.set_age(2);
+
+// cout << s1.get_age() << endl;
+// cout << s1.get_name(123) << endl;
+   
+// }
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++ constructor +++++++++++++++++++++++++++++++++++++++++++
+
+class Customer{
+    string name;
+    int balance;
+    int account_number;
+    
+    // +++++++++++++++++ defatult construtor.+++++++++
+
+     public:
+    Customer(){
+        name = "sachin";
+        balance = 1000;
+        account_number  = 123;
     }
 
-    string get_name(int password){ // Getter, if password is correct then it will return name.
-        if(password == 123){
-            return name;
-        }
-        return "";
+    // ++++++++++++  parameterize constructor. +++++++++
+
+    // Customer(string name, int balance, int account_number){
+    //     this->name = name;
+    //     this->balance = balance;
+    //     this->account_number = account_number;
+    // }
+
+    // +++++++++++++++ constructor overriding. +++++++++++
+
+    Customer(string name, int balance){
+        this->name = name;
+        this->balance = balance;
+        account_number = 789; // default value
     }
 
-    int get_age(){
-        return age;
+    // ++++++++++++++++ inlilne constructor ++++++++++++++++++++
+
+    Customer(string a, int b, int c): name(a),balance(b), account_number(c){
+    }
+
+    // ++++++++++++++ copy constructor ++++++++++++++++++++
+
+    Customer(Customer &B){ // this '&' symbol is representing the passed my memory address of object.
+        name = B.name;
+        balance = B.balance;
+        account_number = B.account_number;
+    }
+
+    void display(){
+        cout << name << " " << balance << " " << account_number << endl;
     }
 };
 
 int main(){
-    Student s1;
-    // s1.name = "john"; // if public then access is direct.
-    // s1.age = 30;
+    Customer A1;
+    Customer A2("hardik", 2000, 456);
+    Customer A3("parveen", 3000);
+    Customer A4(A3); // copy constructor, copy A3 in A4.
+    Customer A5;
+    A5 = A3; //  copy constructor, copy A4 in A5, by diff method.
 
-    // Student s2;
-    // s2.name = "brok";
-    // s2.age = 32;
-
-// if Access modifier are private then GETTER, SETTER used to get and set the data.
-
-s1.set_name("sachin");
-s1.set_age(2);
-
-cout << s1.get_age() << endl;
-cout << s1.get_name(123) << endl;
-   
+    A1.display();
+    A2.display();
+    A3.display();
+    A5.display(); 
 }
