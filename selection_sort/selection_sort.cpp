d6302814f95a8c15b16503a13fe9ad575a34c160
sachin panchal <sachinpanchal9942@gmail.com>
2024-10-22 18:12:30 +0530

    Sorting Practice

--- selection_sort/selection_sort.cpp
+++ selection_sort/selection_sort.cpp
@@ -1,19 +1,49 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int arr[5] = {4,7,3,9,0};
+//     for(int i=0; i<4; i++){
+//         int index = i;
+//         for(int j=i+1; j<5; j++){
+//             if(arr[j] <= arr[index]){
+//                 index = j;
+//             }
+//         }
+//         swap(arr[index], arr[i]);
+//     }
+
+//     // print sorted array.
+//     for(int i=0; i<5; i++){
+//         cout << arr[i] << endl;
+//     }
+// }
+
 #include <iostream>
 using namespace std;
-int main(){
-    int arr[5] = {4,7,3,9,0};
-    for(int i=0; i<4; i++){
-        int index = i;
-        for(int j=i+1; j<5; j++){
-            if(arr[j] <= arr[index]){
-                index = j;
+void selectionSort(int arr[], int size){
+int min;
+    for(int i=0; i<size-1; i++){
+        min = i;
+        for(int j=i+1; j<size; j++){
+            if(arr[j] <= arr[min]){
+                min = j;
             }
         }
-        swap(arr[index], arr[i]);
-    }
+        swap(arr[min], arr[i]);
 
-    // print sorted array.
-    for(int i=0; i<5; i++){
-        cout << arr[i] << endl;
     }
+    cout << "now sorted arry will be printed:"<<endl;
+    for(int i=0; i<size; i++){
+        cout << arr[i]<< endl;
+    }
+}
+
+
+int main(){
+    int arr[5] = {3,5,7,1,7};
+    int size = 5;
+    for(int i=0; i<size; i++){
+        cout << arr[i]<< endl;
+    }
+    selectionSort(arr, size);
 }
