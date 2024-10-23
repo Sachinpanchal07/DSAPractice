84d5ff06d74a844f6378187b3e7586089372b602
sachin panchal <sachinpanchal9942@gmail.com>
2024-10-23 18:12:26 +0530

    Learn Insertion sort

---
+++ Bubble_sort/bubble_sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+void bubbleSort(int arr[], int size){
+    for (int i=1; i<size; i++){
+        bool sorted = false;
+        for(int j=0; j<size-i; j++){ // size-i, b/z in each round last element  is sorted, and we don't need to see that element again.
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+                sorted = true;
+            }
+            if (sorted==true){ // more optimized
+                break; // Means array is already sorted b/z no swaping occured.
+            }
+        }
+    }
+
+    for(int i=0; i<size; i++){
+        cout << arr[i] << endl;
+    }
+}
+
+int main(){
+    int arr[5] = {5,4,3,2,1};
+    int size = 5;
+    cout << "before sorting:" << endl;
+    for(int i=0;i<size;i++){
+        cout << arr[i] << endl;
+    }
+
+    cout << "after Sorting: "<<endl;
+    bubbleSort(arr, size);
+}
---
+++ insertion_sort/insertion_sort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void insertion_sort(int arr[], int size){
+
+    for(int i=1; i<size; i++){
+        int key = arr[i]; // store the key first
+        int j = i - 1;    // store j b/z we have to go from j-1 to j=0.
+        
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j+1] = arr[j]; // shifting the value to the right side.
+            j = j-1;
+        }
+        arr[j+1] = key; // now insert the key at right place.
+        
+    }
+    for(int i=0; i<size; i++){
+        cout<< arr[i] <<endl;
+    }
+}
+
+
+int main(){
+    int arr[5] = {5,4,1,3,2};
+    int size = 5;
+
+    cout << "before sorting: " <<endl;
+    for(int i=0; i<size; i++){
+        cout<< arr[i] <<endl;
+    }
+
+    cout << "after sorting: " << endl;
+
+    insertion_sort(arr, size);
+}
