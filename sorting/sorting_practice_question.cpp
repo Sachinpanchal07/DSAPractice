493a0f06cee1c4222332be09b1db9dbf1c1fdcce
sachin panchal <sachinpanchal9942@gmail.com>
2024-11-11 16:12:26 +0530

    practice sorting questions

---
+++ sorting/sorting_practice_question.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+// ++++++++++++++++++++++ reverse array +++++++++++++++++++++++++++++
+
+// void reverse(int arr[], int size){
+//     int s = 0;
+//     int e = size-1;
+
+//     while (s < e){
+//         swap(arr[s], arr[e]);
+//         s += 1;
+//         e -= 1;
+//     }
+
+//     cout << "after the reverse" << endl;
+
+//     for(int i = 0; i < size ; i++){
+//         cout << arr[i] << endl;
+//     }
+// }
+// int main(){
+// int arr[5] = {4,6,3,5,8};
+// int size = 5;
+// reverse(arr, size);
+
+// }
+
+// ======================= reverse vector =====================================
+
