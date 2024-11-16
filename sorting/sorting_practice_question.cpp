1db7d7ea7a8655fdd248b6e9b497fc4b23908021
sachin panchal <sachinpanchal9942@gmail.com>
2024-11-16 16:50:57 +0530

    Practice question

--- sorting/sorting_practice_question.cpp
+++ sorting/sorting_practice_question.cpp
@@ -66,34 +66,72 @@ using namespace std;
 
 // ===================== merge two sorted array ===================================
 
-void merge(int a[], int size1, int b[], int size2, int c[]){
-    int i=0,j=0,k = 0;
-    while( i < size1 && j < size2){
-        if(a[i] < b[j]){
-            c[k++] = a[i++];
-        }
-        else{
-            c[k++] = b[j++];
-        }
-    }
+// void merge(int a[], int size1, int b[], int size2, int c[]){
+//     int i=0,j=0,k = 0;
+//     while( i < size1 && j < size2){
+//         if(a[i] < b[j]){
+//             c[k++] = a[i++];
+//         }
+//         else{
+//             c[k++] = b[j++];
+//         }
+//     }
 
-    while(i < size1){
-        c[k++] = a[i++];
-    }
-    while(j < size2){
-        c[k++] = b[j++];
-    }
+//     while(i < size1){
+//         c[k++] = a[i++];
+//     }
+//     while(j < size2){
+//         c[k++] = b[j++];
+//     }
 
-    for(int i=0; i<8; i++){
-        cout << c[i] << endl;
-    }
-}
+//     for(int i=0; i<8; i++){
+//         cout << c[i] << endl;
+//     }
+// }
 
 
-int main(){
-    int a[5] = {1,3,5,7,9};
-    int b[3] = {2,4,6};
-    int c[8];
+// int main(){
+//     int a[5] = {1,3,5,7,9};
+//     int b[3] = {2,4,6};
+//     int c[8];
 
-    merge(a, 5, b, 3, c);
-}
+//     merge(a, 5, b, 3, c);
+// }
+
+// ===================================================
+
+// int main(){
+//     int a[5] = {1,0,5,0,9};
+//     int s = 0;
+//     int e = 4;
+//     while(s < e){
+//         if(a[s] == 0 || a[e] == 0){
+//             swap(a[s], a[e]);
+//             s++;
+//             e--;
+//         }
+//     }
+//     for(int i=0; i<4; i++){
+//         cout << a[i]<< " ";
+//     }
+// }
+
+
+// ============================ putting zero's value at right side ============================
+// int main(){
+//     int arr[8] = {2,0,1,0,3,0,0,0};
+//     int size = 8;
+//     for(int i=1; i<size; i++){
+//         int key = arr[i];
+//         int j = i-1;
+//         while(j>=0 && arr[j]==0){
+//             arr[j+1] = arr[j];
+//             j -= 1;
+//         }
+//         arr[j+1] = key;
+//     }
+//     cout << "after sorting: " << endl;
+//     for(int i=0; i<size; i++){
+//         cout << arr[i] << endl;
+//     }
+// }
