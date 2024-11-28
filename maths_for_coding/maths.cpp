7991eacf0198d8009f8adb2b78866bf2a2eaea77
sachin panchal <sachinpanchal9942@gmail.com>
2024-11-28 11:32:58 +0530

    practice question

---
+++ maths_for_coding/maths.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// ================================ sieve of eratosthenes =========================================
+// this is optimize solution for finding prime numbers less then a given number.
+
+// int main(){
+//     int n = 40;
+//     int count = 0;
+//     vector<bool> isPrime(n+1, true);
+
+//     // mark 0,1 false manually
+//     isPrime[0] = isPrime[1] = 0;
+    
+//     // loop from 2 to 40
+//     for(int i=2; i<n; i++){
+//         // if number is true, count++
+//         if(isPrime[i]){
+//             count++;
+//             // mark multiples of number false
+//             for(int j=i*i; j<n; j=j+i){
+//                 isPrime[j] = false;
+//             }
+//         }
+//     }
+//     cout << "number of prime numbers less than "<< n << " is " << count;
+// }
+
+// =================================== finding GDC/HCF =====================================
+
+int main(){
+
+    
+}
