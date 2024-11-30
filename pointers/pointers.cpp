f8e7da06994c06b27ae8aefb4b51855365dbb72c
sachin panchal <sachinpanchal9942@gmail.com>
2024-11-30 23:29:26 +0530

    Learning pointers

--- pointers/pointers.cpp
+++ pointers/pointers.cpp
@@ -3,14 +3,24 @@ using namespace std;
 
 int main(){
     
-    int num = 5;
-    int *ptr = &num;
+    // int num = 5;
+    // int *ptr = &num;
 
     // cout << ptr << endl;
     // cout << *ptr << endl;
     // cout << sizeof(ptr) << endl;
     // cout << sizeof(*ptr) << endl;
 
-    int *q = ptr;
-    cout << *q;
+    // int *q = ptr;
+    // cout << *q;
+
+    // ======================== pointer with int array ========================
+
+    int arr[10] = {2,5,6,4};
+    cout << arr << endl; // address
+    cout << *arr << endl; // 2
+    cout << *(arr+1) << endl; // 5 
+    cout << *(arr) + 1 << endl; // 3
+    int i = 3;
+    cout << i[arr] << endl; // 4 
 }
