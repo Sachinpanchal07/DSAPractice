d59e2bb966378b3b0a35a7140ba947dd72b2a99a
sachin panchal <sachinpanchal9942@gmail.com>
2024-12-01 12:17:28 +0530

    Double pointers

--- pointers/pointers.cpp
+++ pointers/pointers.cpp
@@ -16,11 +16,25 @@ int main(){
 
     // ======================== pointer with int array ========================
 
-    int arr[10] = {2,5,6,4};
-    cout << arr << endl; // address
-    cout << *arr << endl; // 2
-    cout << *(arr+1) << endl; // 5 
-    cout << *(arr) + 1 << endl; // 3
-    int i = 3;
-    cout << i[arr] << endl; // 4 
+    // int arr[10] = {2,5,6,4};
+    // cout << arr << endl; // address
+    // cout << *arr << endl; // 2
+    // cout << *(arr+1) << endl; // 5 
+    // cout << *(arr) + 1 << endl; // 3
+    // int i = 3;
+    // cout << i[arr] << endl; // 4 
+
+    // ========================= double pointers =============================
+    int i = 5;
+    int* ptr = &i;
+    int** ptr2 = &ptr;
+
+    cout << ptr << endl; // print address of i
+    cout << ptr2 << endl; // address of p
+    cout << *ptr2 << endl; // print address of i
+    cout << **ptr2<< endl; // print value of i
+    cout << " ======== address of i is:  " << endl;
+    cout << &i << endl;
+    cout << ptr << endl;
+    cout << *ptr2 << endl;
 }
