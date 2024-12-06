57f4de82e615720832c9677d471d0fa15447d143
sachin panchal <sachinpanchal9942@gmail.com>
2024-12-06 19:30:58 +0530

    sq of sorteda array

--- array/array_qns.cpp
+++ array/array_qns.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // rotate array ....
-
+/*
 void rotate(int nums[], int k, int size){
 
     k = k%size; // must include this line.
@@ -40,3 +40,34 @@ int main(){
         cout << arr[i] << endl;
     }
 }
+*/
+
+// square of sorted array
+/*
+void sq(int *arr,int n){
+    // sq the array;
+    for(int i=0; i<n; i++){
+        arr[i] = arr[i]*arr[i];
+    }
+
+    // sort the array.
+    for(int i=0; i<n-1; i++){
+        int index = i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j] < arr[index]){
+                index = j;
+            }
+        }
+        swap(arr[i],arr[index]);
+    }
+    for(int i=0; i<n; i++){
+        cout << arr[i] << endl;
+    }
+}
+
+int main(){
+    int arr[] = {-4,-1,0,3,10};
+    int n = 5;
+    sq(arr,n);
+}
+*/
