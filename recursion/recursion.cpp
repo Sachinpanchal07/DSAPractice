b09c4bd9580f3a819d2158ca10b790b29a80b75b
sachin panchal <sachinpanchal9942@gmail.com>
2024-12-21 10:34:05 +0530

    permutation using recursion

--- recursion/recursion.cpp
+++ recursion/recursion.cpp
@@ -282,3 +282,34 @@ int main(){
     }
 }
 */
+
+// ============ permutation of array or string =====
+
+/*
+void permutation(vector<int>& nums, vector<vector<int>>& ans, int index){
+    // base case
+    if(index >= nums.size()){
+        ans.push_back(nums);
+    }
+    // process part
+    for(int j = index; j<nums.size(); j++){
+        swap(nums[index], nums[j]);
+        permutation(nums, ans, index+1);
+        // backtrack
+        swap(nums[index], nums[j]);
+    }
+}
+int main(){
+    vector<int> nums = {1,2,3};
+    int index = 0;
+    vector<vector<int>> ans;
+    permutation(nums, ans, index);
+    for(int i=0; i<ans.size(); i++){
+        cout<<"[";
+        for (int j=0; j<ans[i].size(); j++){
+            cout << " " << ans[i][j] << " ";
+        }
+        cout << "] ";
+    }
+}
+*/
