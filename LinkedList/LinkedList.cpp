a0901baa7f3404c9fc1392ba25a067247c42e808
sachin panchal <sachinpanchal9942@gmail.com>
2024-12-23 15:02:38 +0530

    Linked List

---
+++ LinkedList/LinkedList.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next; // create pointer of Node type.
+    Node(int data)
+    {
+        this->data = data; // access the data in object in heap memory.
+        this->next = NULL;
+    }
+};
+
+// insert at head node
+void insertAtHead(Node *&head, int d)
+{
+    Node *node2 = new Node(d);
+    node2 -> next = head;
+    head = node2;
+}
+
+// traverse in singly LL
+void traverse(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+}
+
+int main()
+{
+    // create a dynamic memeory node.
+    Node *node1 = new Node(40);
+    Node *head = node1;
+    traverse(head);
+    cout << endl;
+    insertAtHead(head, 30);
+    traverse(head);
+    cout << endl;
+    insertAtHead(head, 20);
+    traverse(head);
+    cout << endl;
+    insertAtHead(head, 10);
+    traverse(head);
+}
