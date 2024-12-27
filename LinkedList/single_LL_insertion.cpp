94e57b02079f4ff6ba93fb5b288b7ef640a10e78
sachin panchal <sachinpanchal9942@gmail.com>
2024-12-27 17:01:46 +0530

    single LL deletion

--- LinkedList/single_LL_insertion.cpp
+++ LinkedList/single_LL_insertion.cpp
@@ -11,6 +11,14 @@ public:
         this->data = data; // access the data in object in heap memory.
         this->next = NULL;
     }
+    // destructor, no need to call manually, if you write "delete temp" your work is done.
+    // ~Node(){
+    //     // freeup memory.
+    //     if(this->next != NULL){
+    //         delete next;
+    //         this->next = NULL;
+    //     }
+    // }
 };
 //                           =================== insertion ======================
 // insert at head node
@@ -59,6 +67,8 @@ void insertATPosition(Node*& head, Node*& tail, int pos, int d){
     temp -> next = newNode;
 }
 
+//                     ====================== traversal ===================
+
 // traverse in singly LL
 void traverse(Node *head)
 {
@@ -73,9 +83,60 @@ void traverse(Node *head)
 
 //                        ====================== deletion ======================
 
-void deleteNode(){
-    
+// delete the first node.
+void deleteFirstNode(Node*& head, Node*& tail){
+    Node* temp = head;
+    head = head->next;
+    tail = tail->next;
+    delete temp; // freeup memory for temp manually b/z we allocated memory in heap dynamically.
+    return;
 }
+
+// delete last node.
+void deleteLastNode(Node*& head, Node*& tail){
+    Node* temp = head;
+    while(temp->next->next != NULL){
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = NULL;
+    tail = temp;
+    return;
+}
+
+// delete Node of given postion.
+void deleteNode(Node*& head, Node*& tail, int pos){
+    //  delete first node.
+    if(head == NULL){
+        return;
+    }
+
+    Node* temp = head;
+    if(pos == 1){
+        head = head->next;
+        tail = tail->next;
+        // free memory.
+        delete temp; // destructor is called for this.
+    }
+    else{
+        Node* current = head;
+        Node* prev = NULL;
+        int count = 1;
+        while(count < pos){
+            prev = current;
+            current = current->next;
+            count++;
+        }
+        prev->next = current->next;
+        if(prev->next == NULL){
+            tail = prev;
+        }
+        // memory free for current.
+        delete current;
+    }
+    return;
+}
+
 
 int main()
 {
@@ -100,6 +161,13 @@ int main()
 
     // insert at specific postion
     insertATPosition(head, tail, 3, 12);
+    traverse(head);
+
+    // delete operations.
+    cout << "delete operations" << endl;
+    deleteNode(head, tail, 4);
+    // deleteFirstNode(head,tail);
+    // deleteLastNode(head,tail);
     traverse(head);
 
 }
