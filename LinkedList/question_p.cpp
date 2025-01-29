6afd8bb21d102ffb8052f05bfbd214f6a4f27943
sachin panchal <sachinpanchal9942@gmail.com>
2025-01-30 00:18:15 +0530

    LL cycle detection and remove

--- LinkedList/question_p.cpp
+++ LinkedList/question_p.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<map>
 using namespace std;
 
 
@@ -44,3 +45,70 @@ using namespace std;
 //         return false;
 //     }
 // }
+
+// =========================== detect loop in LL ===================== 
+
+// bool detectLoop(Node* head){
+//     if(head == NULL){
+//         return false;
+//     }
+//     map<Node*, bool> visited;
+//     Node* temp = head;
+//     while(temp != NULL){
+//         if(visited[temp] == true){
+//             cout << temp->data << " "; // this will tell which node create the cycle.
+//             cout << "cycle is detected";
+//             return false;
+//         }
+//         temp = temp->next;   
+//     }
+//     return true;
+// }
+
+// ======================= detect loop with slow and fast poiners ==================
+
+/*Node* detectCycle(Node* head){
+    if(head == NULL){
+        return NULL;
+    }
+    Node* slow = head;
+    Node* fast = head;
+
+    while(slow != fast && fast != NULL){
+        fast = fast->next;
+        if(fast->next != NULL){
+            fast = fast->next;
+        }
+        slow = slow->next;
+    }
+    return slow;
+}*/
+
+// =========================== detect which node create cycle ====================
+
+/*bool nodeCreateCycle(Node* head){
+    if (head == NULL){
+        return false;
+    }
+    Node* intersection = detectCycle(head); // call above function.
+    Node* slow = head;
+    while(slow != intersection){
+        slow = slow->next;
+        intersection = intersection->next;
+    }
+    return slow;//this is the point where intersection and slow meets again,and this point is cycle creator
+}*/
+
+// ============================ Remove cycle in LL =======================
+
+/*void removeCycle(Node* head){
+    if(head == NULL){
+        return;
+    }
+    Node* intersect = nodeCreateCycle(head); // call above function.
+    Node* temp = intersect;
+    while(temp->next != intersect){
+        temp = temp-next;
+    }
+    temp-next = NULL; // remove cycle here.
+}*/
