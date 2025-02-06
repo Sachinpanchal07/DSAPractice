1274712116c3b2a78eeb4b9f900913b3e718c6c2
sachin panchal <sachinpanchal9942@gmail.com>
2025-02-06 16:06:32 +0530

    queue reverse

---
+++ queue/queue_qn.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+// ============================ reverse queue =========================
+/*
+queue<int> reverse(queue<int> q){
+    stack<int> s;
+    // build stack
+    while(!q.empty()){
+        int el = q.front();
+        q.pop();
+        s.push(el);
+    }
+
+    // build queue
+    while (!s.empty())
+    {
+        int el = s.top();
+        s.pop();
+        q.push(el);
+    }
+    return q;
+    
+}
+*/
+
+int main(){
+
+}
