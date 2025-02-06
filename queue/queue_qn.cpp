853d34386e272e3f8534ccc2337f6df94655a109
sachin panchal <sachinpanchal9942@gmail.com>
2025-02-06 19:23:42 +0530

    first non-repeating character in stream

--- queue/queue_qn.cpp
+++ queue/queue_qn.cpp
@@ -27,6 +27,7 @@ queue<int> reverse(queue<int> q){
 */
 
 // =============================== reverse k elements of queue =============================
+/*
 queue<int> reverse(queue<int>q, int k){
     stack<int>s;
     for(int i=0; i<k; i++){
@@ -48,6 +49,31 @@ queue<int> reverse(queue<int>q, int k){
         q.push(el);
     }
     return q;
+}
+*/
+// ============================ first non-repeating character in stream ===================
+vector<int> repear(string s){
+    map<char, int>m;
+    queue<char>q;
+    vector<char>v;
+    for(int i=0; i<s.length(); i++){
+        char ch = s[i];
+        m[ch]++;
+        q.push(ch);
+        while(!q.empty()){
+            if(m[q.front()] > 1){ // if frequency greater 1 pop.
+                q.pop();
+            }
+            else{
+                v.push_back(q.front());
+                break;
+            }
+        }
+        if(!q.empty()){
+            v.push_back('#');
+        }
+    }
+    return v;
 }
 int main(){
 
