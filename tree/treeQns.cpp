8db493e9808e34124328dcf1e3defc2606f85328
sachin panchal <sachinpanchal9942@gmail.com>
2025-02-18 18:18:13 +0530

    zig-zag traversal in tree

--- tree/treeQns.cpp
+++ tree/treeQns.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<utility>
+#include<vector>
+#include<queue>
 using namespace std;
 
 class Node {
@@ -199,6 +201,7 @@ bool isIdentical(Node* r1, Node* r2){
 */
 
 // ============================= Sum tree ============================
+/*
 pair<bool,int> sumTree(Node* root){
     // base case
     if(root == NULL){
@@ -232,6 +235,46 @@ pair<bool,int> sumTree(Node* root){
     
     return ans;
 }
+*/
+
+// ================================ zig-zag or spiral traversal =================================
+vector<int>zigZag(Node* root){
+    vector<int>result;
+    queue<Node*>q;
+
+    if(root == NULL){
+        return result;
+    }
+    q.push(root);
+    // maintain flag for detecting direction of traversal.
+    bool leftToRight = true;
+
+    // level order traversal
+    while(!q.empty()){
+        int size = q.size();
+        vector<int>ans(size);
+
+        for(int i=0; i<size; i++){
+            Node* frontNode = q.front();
+            q.pop();
+            int index = leftToRight ? i : size - i - 1; // checking the direction
+            ans[index] = frontNode->data;
+
+            if(frontNode->left){
+                q.push(frontNode->left);
+            }
+            if(frontNode->right){
+                q.push(frontNode->right);
+            }
+        }
+        leftToRight = !leftToRight; // revere the direction
+
+        for(auto i: ans){
+            result.push_back(i);
+        }
+    }
+    return result;
+}
 
 
 
@@ -256,7 +299,13 @@ int main(){
     // cout << isBalanced(root).first << endl;
 
     // sum tree
-    cout << sumTree(root).first;
+    // cout << sumTree(root).first;
+
+    // zig-zag traversal
+    vector<int> a = zigZag(root);
+    for(int i=0; i<a.size(); i++){
+        cout << a[i] << " ";
+    }
 
 
 
